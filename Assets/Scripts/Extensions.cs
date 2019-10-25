124f339f1d52c168c646858e59b1b04125500bba
darcy97 <darcy97zh@gmail.com>
2019-10-25 21:35:13 +0800

    Explore iterator

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class Extensions
+{
+
+    public static IEnumerable<T> EveryNthItem<T> (this IEnumerable<T> sequence, int period)
+    {
+
+        //check null exception
+
+        var count = 0;
+        foreach (var item in sequence)
+        {
+            if (++count % period == 0)
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public static void ForEach<T> (this IEnumerable<T> sequence, Action<T> action)
+    {
+        if (sequence == null)
+            throw new ArgumentNullException (nameof (sequence), "sequence must not be null");
+
+        if (action == null)
+            throw new ArgumentNullException ("Action must not be null");
+
+        foreach (T item in sequence)
+        {
+            action (item);
+        }
+    }
+
+    public static IEnumerable<Tout> TransformForEach<Tin, Tout> (this IEnumerable<Tin> sequence, Func<Tin, Tout> action)
+    {
+
+        if (sequence == null)
+            throw new ArgumentNullException (nameof (sequence), "sequence must not be null");
+
+        if (action == null)
+            throw new ArgumentNullException ("Action must not be null");
+
+        foreach (Tin item in sequence)
+        {
+            yield return action (item);
+        }
+    }
+
+    public static IEnumerable WhereImplement<T> (this IEnumerable<T> sequence, Predicate<T> filterFunc)
+    {
+
+        //check null exception
+
+        foreach (var item in sequence)
+        {
+            if (filterFunc (item))
+                yield return item;
+        }
+    }
+
+}
---
+++ Assets/Scripts/MyIterator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using UnityEngine;
+
+
+
+/// <summary>
+/// //* 迭代器方式 
+/// *可以在使用到序列中某个元素时才创建
+/// *不用先创建整个序列在进行第二步操作 
+/// *在某些操作中 
+/// *生成整个序列开销可能很大 
+/// *例如某些操作可能是对生成的序列进行查找操作（仅查找前几个或一个就停止） 
+/// *那么使用这种这种方式可以有效减小消耗 
+/// *因为这样不会创建出后面的不必要元素 
+/// *或者要对某一序列中的元素进行一系列操作时 
+/// *这种方式的优点就更佳明显
+/// </summary>
+public class MyIterator : MonoBehaviour
+{
+
+    public List<int> Numbers;
+
+    private void Start ()
+    {
+        // Where ();
+        var result = Numbers.Where (x => x < 5);
+        result.TakeWhile (x => x < 5);
+        result.TakeWhile (delegate (int num) { return num < 5; });
+        Transform_cs ();
+    }
+
+    private void Transform_cs ()
+    {
+        Numbers.TransformForEach (x => (x + 10).ToString ()).ForEach (x => print (x));
+    }
+
+    private void EveryN (int n = 2)
+    {
+        Numbers.EveryNthItem (n).ForEach (x => print (x));
+    }
+
+    private void Where ()
+    {
+        var result = Numbers.Where (x => x < 4);
+        result.ForEach (x => print (x));
+    }
+
+}
